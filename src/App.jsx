9b5d313958c4517464ec4f1e333dcb0755a00d43
HOUR <sseanghour33@gmail.com>
2026-02-27 00:35:58 +0700

    update JobCard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import './App.css'
+import CardBusinessComponent from './components/cardbussiness/CardBusinessComponent'
 
 function App() {
-
-
   return (
     <>
-      
+      <CardBusinessComponent />
     </>
   )
 }
