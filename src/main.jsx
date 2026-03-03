98e695b9f0c81ed19cca44d8811c68ddc4402dc7
Tolahhamadabidin <tollahhamadabidin@gmail.com>
2026-03-03 04:42:21 -0500

    bidinn

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+// src/main.jsx
 import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -16,10 +17,11 @@ import LoginPage from './pages/Login.jsx'
 import Home from './pages/Home.jsx'
 import { setupListeners } from '@reduxjs/toolkit/query'
 import CardDetailFreelancer from './components/carddetail/CardDetailFreelancer.jsx'
-import CardDetailBusiness from './components/carddetail/CarddetailBusiness.jsx'
 import ProfileBusinessPage from './pages/ProfileBusiness.jsx'
 import ProfileRouter from './routes/ProfileRouter.jsx'
 import FreelancerPublicProfile from './pages/FreelancerPublicProfile.jsx'
+import DetailWork from './pages/DetailWork.jsx'
+import DetailWorkPage from './pages/DetailWork.jsx'
 
 setupListeners(store.dispatch);
 
@@ -36,15 +38,18 @@ createRoot(document.getElementById('root')).render(
                 <Route path="/findfreelan" element={<FindFreelancers />} />
                 <Route path="/findwork"    element={<FindWork />} />
 
+                
+                <Route path="/jobs/:jobId" element={<DetailWorkPage />} />
+
                 {/* /profile auto-routes to correct page based on userType */}
                 <Route path="/profile" element={<ProfileRouter />} />
                 <Route path="/freelancers/:userId" element={<FreelancerPublicProfile />} />
 
-                <Route path="/profile-business" element={<ProfileBusinessPage />} />
-                <Route path="/services/:serviceId" element={<CardDetailFreelancer />} />
-                <Route path="/jobs/:jobId"         element={<CardDetailBusiness />} />
-                <Route path="/login"               element={<LoginPage />} />
-                <Route path="/register"            element={<Register />} />
+                <Route path="/profile-business"      element={<ProfileBusinessPage />} />
+                <Route path="/services/:serviceId"   element={<CardDetailFreelancer />} />
+               
+                <Route path="/login"                 element={<LoginPage />} />
+                <Route path="/register"              element={<Register />} />
               </Route>
             </Routes>
           </DarkModeProvider>
